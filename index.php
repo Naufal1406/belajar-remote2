079cd555507485662bcf1f7c07e41058e4802ade
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-11-03 21:16:51 +0700

    menghapus lorem

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>git remote 2</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat non aliquam, at eum quos incidunt ab corporis, omnis dolor voluptas deleniti veritatis eius magni ea id ipsa voluptatem blanditiis.</p>
+<h1>Hello World!</h1>
 </body>
 </html>
