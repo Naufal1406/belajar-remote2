1c2148f7dfe18582c79c5a2386d245d68c5150be
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-11-03 21:15:00 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote 2</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat non aliquam, at eum quos incidunt ab corporis, omnis dolor voluptas deleniti veritatis eius magni ea id ipsa voluptatem blanditiis.</p>
+</body>
+</html>
